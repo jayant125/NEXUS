1501c8aae2d28e77984056da7c367efbbbebd2ce
JAYANT KUMAR <jayantkumar62061@gmail.com>
2024-11-22 22:15:42 +0530

    31

---
+++ 31_Day-JS/frist.js
@@ -0,0 +1,81 @@
+// Comparison operator
+// number to number
+
+// let a1 = 1;
+// let a2 = 2;
+// console.log(a1 == a2);
+// console.log(a1 > a2);
+// == both are equal
+// < less than, > greater than, <= less than or equal to, >= greater than or equal to, != not equal to, 
+
+// let num = 10;
+// let str = "10";
+// console.log(num==str);
+// Type conversion hga string to number
+
+// let a1 = 10;
+// let str1 = "10";
+// console.log(a1<str1)
+
+// === type check then compare the value
+// console.log(a1===str1);
+
+// let a1 = 10;
+// let a2 = 10;
+// console.log(a1===a2);
+
+// null == undifined // true
+// null === undifined  // false
+
+// console.log(null == undefined);
+// console.log(null === undefined);
+// null  can only be equivalent to undifined
+
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null > 0);
+// console.log(null != 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// console.log(undefined != 0);
+// console.log(NaN == NaN)
+
+// let st3 = "jayant";
+// let st4 = "anshul";
+// console.log(Number(st3) == Number(st4));
+
+// let abc1 = 123;
+// let abc2 = "123";
+// let abc3 = 123;
+// console.log(abc1 == abc2 == abc3);
+
+let age = 18;
+let money = 420;
+console.log(age >= 18 && money >= 200);
+
+console.log(age > 10 || money > 200);
+
+// Bit wise operators
+// & (bitwise AND)
+// | (bitwise OR)
+// ~ (bitwise NOT)
+// ^ (bitwise XOR)
+// << (bitwise left shift)
+// >> (bitwise right shift)
+// >>> (bitwise unsigned right shift)
+
+let a = 10;
+console.log(4&5);
+console.log(4|5);
+console.log(~4);
+console.log(4^5);
+console.log(5<<3);
+// 5 * 2^3 = 40 // 5 multiply by 2 power 3
+// 101.0000000000
+// 101000.0000000
+
+console.log(20>>2);
+// 20 / 2^2 = 5 // 20 divide by 2 power 2
+// 10100.0000000
+// 101.0000000000
